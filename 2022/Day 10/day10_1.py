7048dc77b2c799211055c8b4fba6d295c102fa4e
Stronkness <andrefrisk98@gmail.com>
2022-12-11 20:38:09 +0100

    Day 10 done

---
+++ 2022/Day 10/day10_1.py
@@ -0,0 +1,27 @@
+X = 1
+signal_strength = 0
+cycle = 0
+
+instructions = open('input').read().split('\n')
+
+for i, execution in enumerate(instructions, 1):
+    if execution == 'noop':
+        cycle += 1
+        if cycle % 40 == 20: # Thresholds for signal_strength
+            signal_strength += X*cycle
+
+    else: # addx
+        _, V = execution.split(" ")
+
+        # Two clock cycles for this instruction
+        cycle += 1
+        if cycle % 40 == 20: # Thresholds for signal_strength
+            signal_strength += X*cycle
+
+        cycle += 1
+        if cycle % 40 == 20: # Thresholds for signal_strength
+            signal_strength += X*cycle
+
+        X += int(V)
+
+print(signal_strength)
---
+++ 2022/Day 10/day10_2.py
@@ -0,0 +1,26 @@
+X = 1
+cycle = 0
+
+X_progress = [X]
+
+instructions = open('input').read().split('\n')
+
+for i, execution in enumerate(instructions, 1):
+    if execution == 'noop':
+        X_progress.append(X)
+
+    else: # addx
+        _, V = execution.split(" ")
+
+        X_progress.extend([X,X]) # add two X's because of two clock cycles
+        X += int(V)
+
+for y in range(6): # rows
+    crt = ''
+    for x in range(40): # cols
+        cycle = x + y * 40 # current index in cols but the row in y*40 indicated the rows divisble by 40. For e.g y*2 = 80 which means row 2
+        if abs(X_progress[cycle + 1] - x) <= 1:
+            crt += '#'
+        else:
+            crt += '.'
+    print(crt)
