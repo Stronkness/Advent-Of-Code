510576c971f8bddff4472c3d307d29dab037022d
Stronkness <andrefrisk98@gmail.com>
2023-12-09 01:38:14 +0100

    Day 8 done

--- 2023/Day 8/day08.py
+++ 2023/Day 8/day08.py
@@ -1,21 +1,21 @@
 from collections import defaultdict
+import math
 
 def handle_data():
-    global path_count, instructions_length, paths, instructions
+    global instructions_length, paths, instructions
     data = open('input', 'r').read().splitlines()
     instructions = data[0]
     pathways = data[2:]
 
-    path_count = 0
     instructions_length = len(instructions)
     paths = defaultdict(list)
     [paths[path.split()[0]].extend(path[path.find("(") + 1:path.find(")")].split(', ')) for path in pathways]
 
-def compute_paths():
-    global path_count
-    current_path = 'AAA'
+def compute_paths(start):
+    path_count = 0
+    current_path = start
     inst_count = 0
-    while current_path != 'ZZZ':
+    while not current_path.endswith('Z'):
         dir = instructions[inst_count]
         if dir == "R":
             current_path = paths[current_path][1]
@@ -28,6 +28,12 @@ def compute_paths():
         if inst_count == instructions_length:
             inst_count = 0
 
+    return path_count
+
 handle_data()
-compute_paths()
-print(path_count)
+print(compute_paths('AAA')) # Part 1
+steps = 1 # Neede for doing LCM maths
+for path in paths:
+    if path.endswith('A'):
+        steps = math.lcm(steps, compute_paths(path))
+print(steps) # Part 2
