ef2df357277ad2efa344f9d0bd47d440f73992e1
Stronkness <andrefrisk98@gmail.com>
2023-12-29 12:16:00 +0100

    Day 22 done, i see the end of the suffering

---
+++ 2023/Day 22/day22.py
@@ -0,0 +1,81 @@
+from copy import deepcopy
+
+def prepare_data():
+    with open('input', 'r') as file:
+        lines = file.read().strip().splitlines()
+
+    prepared_bricks = []
+    for index, line in enumerate(lines):
+        stripped_line = line.strip()
+        brick_coords1, brick_coords2 = stripped_line.split('~')
+        x1, y1, z1 = map(int, brick_coords1.split(','))
+        x2, y2, z2 = map(int, brick_coords2.split(','))
+
+        brick_name = str(index)
+        prepared_bricks.append((x1, y1, z1, x2, y2, z2, brick_name))
+    
+    return prepared_bricks
+
+def is_brick_supported(brick, world):
+    x_start, y_start, z_start, x_end, y_end, _, _ = brick
+    support_level = z_start - 1
+
+    if support_level == 0:
+        return True
+
+    for x in range(x_start, x_end + 1):
+        for y in range(y_start, y_end + 1):
+            if (x, y, support_level) in world:
+                return True
+
+    return False
+
+def apply_gravity(bricks):
+    bricks_fell = False
+    updated_bricks = []
+    support_surface = set()
+
+    for (x_start, y_start, z_start, x_end, y_end, z_end, brick_name) in bricks:
+        for x in range(x_start, x_end + 1):
+            for y in range(y_start, y_end + 1):
+                support_surface.add((x, y, z_end))
+
+    for brick in bricks:
+        x_start, y_start, z_start, x_end, y_end, z_end, brick_name = brick
+
+        if not is_brick_supported(brick, support_surface):
+            bricks_fell = True
+            z_start -= 1
+            z_end -= 1
+            updated_brick = (x_start, y_start, z_start, x_end, y_end, z_end, brick_name)
+            updated_bricks.append(updated_brick)
+        else:
+            updated_bricks.append(brick)
+
+    return bricks_fell, updated_bricks
+
+def calculate_disintegration(bricks):
+    disintegration_count = 0
+
+    for index, _ in enumerate(bricks):
+        bricks_copy = deepcopy(bricks)
+        del bricks_copy[index]
+        bricks_fell, _ = apply_gravity(bricks_copy)
+
+        if not bricks_fell:
+            disintegration_count += 1
+
+    return disintegration_count
+
+def main():
+    bricks = prepare_data()
+    did_fall = True
+    while did_fall:
+        bricks = sorted(bricks, key=lambda x: x[2])
+        did_fall, bricks = apply_gravity(bricks)
+
+    print(calculate_disintegration(bricks))
+
+
+if __name__ == '__main__':
+    main()
