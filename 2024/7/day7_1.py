122913b5709b2aa672fbd5d88910261b684f25c6
André Frisk <50335484+Stronkness@users.noreply.github.com>
2024-12-11 08:45:24 +0100

    AoC24: Day 7 done

---
+++ 2024/7/day7_1.py
@@ -0,0 +1,18 @@
+def valid_calculation(target, nums):
+    if len(nums) == 1: return nums[0] == target
+    if valid_calculation(target, [nums[0] + nums[1]] + nums[2:]): return True
+    if valid_calculation(target, [nums[0] * nums[1]] + nums[2:]): return True
+    return False
+
+input = open("large.in").read().split("\n")
+result = 0
+for line in input:
+    target, nums = line.split(":")
+    target = int(target)
+    nums = list(map(int, nums.split()))
+    valid = valid_calculation(target, nums)
+
+    if valid:
+        result += target
+
+print(result)
